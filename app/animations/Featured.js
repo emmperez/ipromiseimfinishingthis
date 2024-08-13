6121b09b6b3580356a93d9bb5c88c2c6d1a77da4
emmanuel <emmanueleperez@icloud.com>
2024-08-13 19:37:39 -0400

    Added page conditionals

--- app/animations/Featured.js
+++ app/animations/Featured.js
@@ -5,54 +5,56 @@ export default class Featured {
 
     animateFeatured() {
         document.addEventListener("DOMContentLoaded", () => {
-            const items = document.querySelectorAll('.item');
+            if (document.body.classList.contains('home')) {
+                const items = document.querySelectorAll('.item');
 
-            items.forEach((item) => {
-                item.addEventListener('mouseenter',
-                shuffleAnimation);
-            });
+                items.forEach((item) => {
+                    item.addEventListener('mouseenter',
+                    shuffleAnimation);
+                });
 
-            function getRandomCharacter() {
-                const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-                return chars[Math.floor(Math.random() * chars.length)];
-            }
-
-            function shuffleAnimation(event) {
-                const target = event.currentTarget;
-                
-                if (target.dataset.animating) {
-                    return;
+                function getRandomCharacter() {
+                    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                    return chars[Math.floor(Math.random() * chars.length)];
                 }
 
-                target.dataset.animating = true;
-
-                const words = target.querySelectorAll('.word');
-                const originalWords = Array.from(words).map((word) => word.textContent);
-
-                let shuffles = 0;
-                const maxShuffles = 10;
-                const intervalDuration = 700 / maxShuffles;
-
-                let animationInterval = setInterval(() => {
-                    if (shuffles >= maxShuffles) {
-                        clearInterval(animationInterval);
-                        words.forEach((word, index) => {
-                            word.textContent = originalWords[index];
-                        });
-
-                        delete target.dataset.animating;
-                    } else {
-                        words.forEach((word) => {
-                            const length = word.textContent.length;
-                            let shuffledText = "";
-                            for (let i = 0; i < length; i++) {
-                                shuffledText += getRandomCharacter();
-                            }
-                            word.textContent = shuffledText;
-                        });
-                        shuffles++;
+                function shuffleAnimation(event) {
+                    const target = event.currentTarget;
+                    
+                    if (target.dataset.animating) {
+                        return;
                     }
-                }, intervalDuration)
+
+                    target.dataset.animating = true;
+
+                    const words = target.querySelectorAll('.word');
+                    const originalWords = Array.from(words).map((word) => word.textContent);
+
+                    let shuffles = 0;
+                    const maxShuffles = 10;
+                    const intervalDuration = 700 / maxShuffles;
+
+                    let animationInterval = setInterval(() => {
+                        if (shuffles >= maxShuffles) {
+                            clearInterval(animationInterval);
+                            words.forEach((word, index) => {
+                                word.textContent = originalWords[index];
+                            });
+
+                            delete target.dataset.animating;
+                        } else {
+                            words.forEach((word) => {
+                                const length = word.textContent.length;
+                                let shuffledText = "";
+                                for (let i = 0; i < length; i++) {
+                                    shuffledText += getRandomCharacter();
+                                }
+                                word.textContent = shuffledText;
+                            });
+                            shuffles++;
+                        }
+                    }, intervalDuration)
+                }
             }
         });
     }
--- app/animations/Upcoming.js
+++ app/animations/Upcoming.js
@@ -4,116 +4,121 @@ export default class Upcoming {
     constructor() {
         this.animateUpcoming()
     }
+    
     animateUpcoming() {
        document.addEventListener("DOMContentLoaded", function() {
-            const imageSources = [
-                '/impulse-upcoming-1.jpg',
-                '/impulse-upcoming-2.jpg',
-                '/impulse-upcoming-3.jpg',
-                '/impulse-upcoming-4.jpg',
-                '/impulse-upcoming-5.jpg'
-            ]
+            if (document.body.classList.contains('upcoming')) {
 
-            const menuItems = document.querySelectorAll(".menu-item")
+                const imageSources = [
+                    '/impulse-upcoming-1.jpg',
+                    '/impulse-upcoming-2.jpg',
+                    '/impulse-upcoming-3.jpg',
+                    '/impulse-upcoming-4.jpg',
+                    '/impulse-upcoming-5.jpg'
+                ]
+                
+                const menuItems = document.querySelectorAll(".menu-item")
 
-            menuItems.forEach((item) => {
-                    const copyElements = item.querySelectorAll(".info, .name, .tag");
+                menuItems.forEach((item) => {
+                        const copyElements = item.querySelectorAll(".info, .name, .tag");
 
-                    copyElements.forEach((div) => {
-                        const copy = div.querySelector("p"); 
+                        copyElements.forEach((div) => {
+                            const copy = div.querySelector("p"); 
 
-                        if (copy) {
-                            const duplicateCopy = document.createElement("p");
-                            duplicateCopy.textContent = copy.textContent;
-                            div.appendChild(duplicateCopy);
-                        }
-                    })
-            })
-
-            const appendImages = (src) => {
-                    const preview1 = document.querySelector(".preview-img-1");
-                    const preview2 = document.querySelector(".preview-img-2");
-
-                    const img1 = document.createElement("img");
-                    const img2 = document.createElement("img");
-
-                    img1.src = src;
-                    img1.style.clipPath = "polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%)";
-
-                    img2.src = src;
-                    img2.style.clipPath = "polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%)";
-
-                    preview1.appendChild(img1);
-                    preview2.appendChild(img2);
-
-                    gsap.to([img1, img2], {
-                        clipPath: "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)", 
-                        duration: 1, 
-                        ease: "power3.out", 
-                        onComplete: function () {
-                            removeExtraImages(preview1);
-                            removeExtraImages(preview2);
-                        },
-                    })
-            };
-
-            function removeExtraImages(container) {
-                    while (container.children.length > 10) {
-                        container.removeChild(container.firstChild);
-                    }
-            }
-
-            document.querySelectorAll(".menu-item").forEach((item, index) => {
-                    item.addEventListener("mouseover", () => {
-                        mouseOverAnimation(item);
-                        appendImages(imageSources[index])
-                    })
-
-                    item.addEventListener("mouseout", () => {
-                        mouseOutAnimation(item);
-                    });
-            });
-
-            const mouseOverAnimation = (elem) => {
-                    gsap.to(elem.querySelectorAll("p:nth-child(1)"), {
-                        top: "-100%",
-                        duration: 0.3,
-                    })
-                    gsap.to(elem.querySelectorAll("p:nth-child(2)"), {
-                        top: "0%",
-                        duration: 0.3,
-                    })
-            }
-
-            const mouseOutAnimation = (elem) => {
-                    gsap.to(elem.querySelectorAll("p:nth-child(1)"), {
-                        top: "0%",
-                        duration: 0.3,
-                    })
-                    gsap.to(elem.querySelectorAll("p:nth-child(2)"), {
-                        top: "-100%",
-                        duration: 0.3,
-                    })
-            }
-
-            // document.querySelector(".menu").addEventListener("mouseout", function() {
-            //     gsap.to(".preview-img img", {
-            //         clipPath: "polygon(0% 0%, 100% 0, 100% 0%, 0% 0%", 
-            //         duration: 1, 
-            //         ease: "power3.out",
-            //     })
-            // })
-
-            document.addEventListener("mousemove", function (e) {
-                const preview = document.querySelector(".preview");
-
-                gsap.to(preview, {
-                    x: e.clientX + 300, 
-                    y: e.clientY, 
-                    duration: 1,
-                    ease: "power3.out",
+                            if (copy) {
+                                const duplicateCopy = document.createElement("p");
+                                duplicateCopy.textContent = copy.textContent;
+                                div.appendChild(duplicateCopy);
+                            }
+                        })
                 })
-            })
+
+                const appendImages = (src) => {
+                        const preview1 = document.querySelector(".preview-img-1");
+                        const preview2 = document.querySelector(".preview-img-2");
+
+                        const img1 = document.createElement("img");
+                        const img2 = document.createElement("img");
+
+                        img1.src = src;
+                        img1.style.clipPath = "polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%)";
+
+                        img2.src = src;
+                        img2.style.clipPath = "polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%)";
+
+                        preview1.appendChild(img1);
+                        preview2.appendChild(img2);
+
+                        gsap.to([img1, img2], {
+                            clipPath: "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)", 
+                            duration: 1, 
+                            ease: "power3.out", 
+                            onComplete: function () {
+                                removeExtraImages(preview1);
+                                removeExtraImages(preview2);
+                            },
+                        })
+                };
+
+                function removeExtraImages(container) {
+                        while (container.children.length > 10) {
+                            container.removeChild(container.firstChild);
+                        }
+                }
+
+                document.querySelectorAll(".menu-item").forEach((item, index) => {
+                        item.addEventListener("mouseover", () => {
+                            mouseOverAnimation(item);
+                            appendImages(imageSources[index])
+                        })
+
+                        item.addEventListener("mouseout", () => {
+                            mouseOutAnimation(item);
+                        });
+                });
+
+                const mouseOverAnimation = (elem) => {
+                        gsap.to(elem.querySelectorAll("p:nth-child(1)"), {
+                            top: "-100%",
+                            duration: 0.3,
+                        })
+                        gsap.to(elem.querySelectorAll("p:nth-child(2)"), {
+                            top: "0%",
+                            duration: 0.3,
+                        })
+                }
+
+                const mouseOutAnimation = (elem) => {
+                        gsap.to(elem.querySelectorAll("p:nth-child(1)"), {
+                            top: "0%",
+                            duration: 0.3,
+                        })
+                        gsap.to(elem.querySelectorAll("p:nth-child(2)"), {
+                            top: "-100%",
+                            duration: 0.3,
+                        })
+                }
+
+                // document.querySelector(".menu").addEventListener("mouseout", function() {
+                //     gsap.to(".preview-img img", {
+                //         clipPath: "polygon(0% 0%, 100% 0, 100% 0%, 0% 0%", 
+                //         duration: 1, 
+                //         ease: "power3.out",
+                //     })
+                // })
+
+                document.addEventListener("mousemove", function (e) {
+                    const preview = document.querySelector(".preview");
+
+                    gsap.to(preview, {
+                        x: e.clientX + 300, 
+                        y: e.clientY, 
+                        duration: 1,
+                        ease: "power3.out",
+                    })
+                })
+
+            }
         })
     }
 }
